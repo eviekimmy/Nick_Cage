23e67fec1093942228eda0787d1ac7aa145296d9
eviekimmy <103000039+eviekimmy@users.noreply.github.com>
2022-08-15 21:52:08 -0700

    To CSV
    Changed from printing items into console into writing items into CSV file.

--- Nick_Cage.py
+++ Nick_Cage.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import csv
 
 # pull table from rotton tomatoes website
 url = 'https://www.rottentomatoes.com/celebrity/nicolas_cage'
@@ -8,14 +9,14 @@ req = requests.get(url)
 soup = BeautifulSoup(req.text, 'html.parser')
 main_table = soup.find_all('table')[0]
 
-critics = []
-audience = []
-for row in main_table.find('tbody').find_all('tr'):
-    score_cols = row.find_all('td', class_='celebrity-filmography__score-content')
-    # print(score_cols[0].find_all('span', class_='icon__tomatometer-score'),
-    #  score_cols[1].find_all('span', class_='icon__tomatometer-score'), '\n')
-    for i in score_cols[0].find_all('span', class_='icon__tomatometer-score'):
-        critics.append(i.text)
-    for i in score_cols[1].find_all('span', class_='icon__tomatometer-score'):
-        audience.append(i.text)
-print(critics, '\n', audience)
+# write the critic and audience score to Nick_Cage.csv
+with open('Nick_Cage.csv', 'w') as csvfile:
+    csvwriter = csv.writer(csvfile)
+
+    for row in main_table.find('tbody').find_all('tr'):
+        score_cols = row.find_all('td', class_='celebrity-filmography__score-content')
+        critics = score_cols[0].find_all('span', class_='icon__tomatometer-score')
+        audiences = score_cols[1].find_all('span', class_='icon__tomatometer-score')
+
+        for i,j in zip(critics, audiences):
+            csvwriter.writerow([i.text,j.text])
