ee385624583c452a263397f403d8c453e59df708
eviekimmy <103000039+eviekimmy@users.noreply.github.com>
2022-08-15 22:38:12 -0700

    Updated Analysis
    ^

--- Nick_Cage.R
+++ Nick_Cage.R
@@ -3,10 +3,15 @@ Nick <- read.csv('/Users/eve/Documents/GitHub/Nick_Cage/Nick_Cage.csv')
 critic = Nick$critic
 audience = Nick$audience
 
+# visual analysis
+library(dplyr)
+plot(density(critic), main ='critic')
+plot(density(audience), main ='audience')
 
 boxplot(Nick)
 boxplot(critic-audience)
 
+# summary statistics
 summary(critic)
 summary(audience)
 summary(critic-audience)
@@ -14,4 +19,5 @@ sd(critic)
 sd(audience)
 sd(critic - audience)
 
+# NOTE: Not normal distributions, need another form of analysis
 t.test(x=critic, y=audience, alternative='two.sided', paired=T, conf.level=.8)
