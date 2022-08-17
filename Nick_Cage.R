21328369f79eba89b06d9f460be7057990543d57
eviekimmy <103000039+eviekimmy@users.noreply.github.com>
2022-08-16 20:57:37 -0700

    Updated Analysis
    ^

--- Nick_Cage.R
+++ Nick_Cage.R
@@ -1,23 +1,36 @@
-# importing all the data
+############# importing all the data #############
+
 Nick <- read.csv('/Users/eve/Documents/GitHub/Nick_Cage/Nick_Cage.csv')
 critic = Nick$critic
 audience = Nick$audience
 
-# visual analysis
+############# visual analysis #############
+
 library(dplyr)
 plot(density(critic), main ='critic')
 plot(density(audience), main ='audience')
+# ^ critic and audience scores appear to follow a bimodal distribution
+
+library("car")
+qqPlot(critic - audience)
+# ^ the difference appears to follow a normal distribution
 
 boxplot(Nick)
 boxplot(critic-audience)
 
-# summary statistics
+############# summary statistics #############
+
 summary(critic)
 summary(audience)
-summary(critic-audience)
+summary(critic-audience) # mean: -5.02
 sd(critic)
 sd(audience)
-sd(critic - audience)
+sd(critic - audience) # sigma: 20.540
 
-# NOTE: Not normal distributions, need another form of analysis
-t.test(x=critic, y=audience, alternative='two.sided', paired=T, conf.level=.8)
+############# analysis #############
+
+# Z test to determine whether critics and audiences rate Nicholas Cage films
+# differently with 5% significance level
+pnorm(-5.02,0,20.540) # p-val = .403, no significant difference between critic
+                      # and audience ratings
+
